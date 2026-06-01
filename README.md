9f0d5b1d1a41d01c376964f35a4b18f362bbd218
Devesh Singh <devesh8979276150@gmail.com>
2026-06-01 19:43:37 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# devesh-portfolio
+
+Deploy:
+1. Upload to GitHub
+2. Import repo in Vercel
+3. Deploy
---
+++ layout.js
@@ -0,0 +1,5 @@
+
+export const metadata={title:'Devesh Singh Portfolio'};
+export default function RootLayout({children}){
+ return <html><body>{children}</body></html>
+}
